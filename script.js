121de1aa11ac2aed9de6004275ccf8ce9e9b0360
Shifatur-Rahman <rjnovel28@gmail.com>
2022-06-13 17:59:47 +0600

    Unlimited Parameters

---
+++ script.js
@@ -0,0 +1,20 @@
+//  Unlimited Parameters by rest operators
+
+function myFun(...novel){
+    console.log(novel);
+}
+
+myFun(4,5,10,15,20,'Novel',true);
+
+
+
+
+//  Unlimited Parameters by arguments
+
+// function myFun(){
+//     console.log(arguments);
+// }
+
+// myFun(4,5,10,15,20,'Novel',true);
+
+
